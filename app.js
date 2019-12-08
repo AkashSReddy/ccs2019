eedcec5cdf16aea1ba332c934c7809045855cb58
RajDeshmukh99 <rajvardhan.sudhakar2017@vitstudent.ac.in>
2019-12-08 17:04:15 +0530

    domains added

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ require("dotenv").config();
 
 mongoose.connect(
   process.env.MONGO_URI,
-  { useNewUrlParser: true, useFindAndModify: false },
+  { useNewUrlParser: true, useFindAndModify: false , useUnifiedTopology: true},
   err => {
     if (!err) console.log("Connection successful");
   }
